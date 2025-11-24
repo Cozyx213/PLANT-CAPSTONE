458211cf42ad732f177765af9df8a6a6772919df
rusperres <jairusjaspervc@gmail.com>
2025-11-24 14:06:18 +0000

    Action.java: finished

---
+++ src/main/java/com/plantfarmlogger/model/Action.java
@@ -0,0 +1,14 @@
+public enum Action {
+    FERTILIZE("fertilize"),
+    WATER("water"),
+    AERATE("aerate"),
+    CUT("cut")
+
+    private final String value;
+
+    Action(String action){
+        this.value = value;
+    }
+
+    public String getValue(){return this.value;}
+}
