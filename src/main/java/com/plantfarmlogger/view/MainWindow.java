a5144520adeb740ea0cc89e6669e495fdcc93c91
Jake <bajentingjackie@gmail.com>
2025-11-24 15:06:10 +0800

    added ugly login and logincontroller

--- src/main/java/com/plantfarmlogger/view/MainWindow.java
+++ src/main/java/com/plantfarmlogger/view/MainWindow.java
@@ -1,23 +1,10 @@
 package com.plantfarmlogger.view;
 
 import javax.swing.*;
-import java.awt.*;
 
 public class MainWindow extends JFrame {
     public MainWindow() {
-        super("PLAnt capstone");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(600, 400);
-        setLocationRelativeTo(null);
-
-        JPanel p = new JPanel(new BorderLayout());
-        JLabel l = new JLabel("Hello — this is the UI", SwingConstants.CENTER);
-        p.add(l, BorderLayout.CENTER);
-
-        JButton b = new JButton("Click me");
-        b.addActionListener(e -> JOptionPane.showMessageDialog(this, "Button clicked"));
-        p.add(b, BorderLayout.SOUTH);
-
-        setContentPane(p);
+        LoginForm lf = new LoginForm();
+        lf.setVisible(true);
     }
 }
