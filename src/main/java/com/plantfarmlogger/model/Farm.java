490b7c834db0bc4ec3d24ecade34b096165ea8ef
rusperres <jairusjaspervc@gmail.com>
2025-11-24 13:58:24 +0000

    Farm.java: fields, constructor, getters and setters, empty methods

--- src/main/java/com/plantfarmlogger/model/Farm.java
+++ src/main/java/com/plantfarmlogger/model/Farm.java
@@ -2,5 +2,37 @@
 public class Farm {
     private String name;
     private int size;
+    private CropBed cropBed;
 
+    public Farm(String name, int size) {
+        this.name = name;
+        this.size = size;
+    }
+
+    public void addCropBed(CropBed cropBed){
+        this.cropBed = cropBed;
+    }
+    public int getSize() {
+        return size;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public CropBed getCropBed() {
+        return cropBed;
+    }
+
+    public void setCropBed(CropBed cropBed) {
+        this.cropBed = cropBed;
+    }
 }
