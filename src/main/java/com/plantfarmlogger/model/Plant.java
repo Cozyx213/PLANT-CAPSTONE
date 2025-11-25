90431cb5772263e74fa6aa0f5e12f520bded36b8
rusperres <jairusjaspervc@gmail.com>
2025-11-25 07:02:30 +0000

    checkout to main

--- src/main/java/com/plantfarmlogger/model/Plant.java
+++ src/main/java/com/plantfarmlogger/model/Plant.java
@@ -1,6 +1,36 @@
 package com.plantfarmlogger.model;
 
-public class Plant {
+import com.plantfarmlogger.model.interfaces.*;
+
+public class Plant implements Fertilizable, Waterable,
+        Aeratable, Cuttable{
     private String name;
     private int age;
+
+    public Plant(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public void cut() {}
+
+    @Override
+    public
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
