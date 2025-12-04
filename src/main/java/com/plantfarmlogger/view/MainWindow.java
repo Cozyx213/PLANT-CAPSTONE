fe291cc61518c8060459f7f187d5dbdd40e638e2
Harold Shichiya <shichiya.amistad@gmail.com>
2025-12-04 13:50:17 +0800

    refactored to latest changes

--- src/main/java/com/plantfarmlogger/view/MainWindow.java
+++ src/main/java/com/plantfarmlogger/view/MainWindow.java
@@ -10,22 +10,23 @@ public class MainWindow extends JFrame {
     private final AppNavigator navigator;
 
     public MainWindow() {
-
         setTitle("AniCore Lite");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(1280, 720);
         setLocationRelativeTo(null);
-
         setResizable(true);
 
+        // layout
         cardLayout = new CardLayout();
         mainPanel = new JPanel(cardLayout);
         add(mainPanel);
 
+        // navigation
         navigator = new AppNavigator(mainPanel, cardLayout);
 
+        // first view will always be login
+        // wala pay user kay molog-in pa man
         navigator.showLogin();
-
     }
 
     public AppNavigator getNavigator() {
